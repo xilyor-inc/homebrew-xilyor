e7c072449f45ba74688f72d5f4a112edb35964a9
Xilyor Owner <135472744+xilyor-owner@users.noreply.github.com>
2025-03-22 23:21:03 +0100

    Update networkstat.rb
    update the url and the home page url

--- Formula/networkstat.rb
+++ Formula/networkstat.rb
@@ -1,7 +1,7 @@
 class Networkstat < Formula
   desc "CLI tool to monitor network interfaces with pretty emoji output"
-  homepage "https://github.com/Xilyor/networkstat"
-  url "https://github.com/Xilyor/networkstat/archive/refs/tags/v1.3.19.tar.gz"
+  homepage "https://github.com/mohamedelleuch777/NetworkStat"
+  url "https://github.com/mohamedelleuch777/NetworkStat/archive/refs/tags/v1.3.19.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
   license "Apache-2.0"
 
