f01da08696d60cdf65ba78a2afb16b8323fed55d
Xilyor Owner <135472744+xilyor-owner@users.noreply.github.com>
2025-03-22 23:29:28 +0100

    Update networkstat.rb
    update the sha256

--- Formula/networkstat.rb
+++ Formula/networkstat.rb
@@ -2,7 +2,7 @@ class Networkstat < Formula
   desc "CLI tool to monitor network interfaces with pretty emoji output"
   homepage "https://github.com/mohamedelleuch777/NetworkStat"
   url "https://github.com/mohamedelleuch777/NetworkStat/archive/refs/tags/v1.3.19.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "5bad19cc1716a8a1b0014fe2c8cf4c757a0b8fb2df8e2e7b4b0274c6d4da9215"
   license "Apache-2.0"
 
   depends_on "python@3.11"
