caeadc3a74318806c2372f547c39909454369338
Xilyor Owner <135472744+xilyor-owner@users.noreply.github.com>
2025-03-22 21:55:15 +0000

    first commit

---
+++ Formula/networkstat.rb
@@ -0,0 +1,18 @@
+class Networkstat < Formula
+  desc "CLI tool to monitor network interfaces with pretty emoji output"
+  homepage "https://github.com/Xilyor/networkstat"
+  url "https://github.com/Xilyor/networkstat/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  license "Apache-2.0"
+
+  depends_on "python@3.11"
+
+  def install
+    bin.install "networkstat.py" => "networkstat"
+    man1.install "networkstat.1"
+  end
+
+  test do
+    system "#{bin}/networkstat", "--version"
+  end
+end
