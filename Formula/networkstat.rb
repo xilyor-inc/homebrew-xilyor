c1bc7b5a2e16511b4e08189b843988512a2c118a
mohamedelleuch777 <usher7med@gmail.com>
2025-03-23 01:11:01 +0100

    Fix: Use Homebrew's recommended way for Python formulae in third-party taps

--- Formula/networkstat.rb
+++ Formula/networkstat.rb
@@ -1,9 +1,4 @@
-require "language/python"
-require "language/python/virtualenv"
-
 class Networkstat < Formula
-  include Language::Python::Virtualenv
-
   desc "CLI tool to monitor network interfaces with pretty emoji output"
   homepage "https://github.com/mohamedelleuch777/NetworkStat"
   url "https://github.com/mohamedelleuch777/NetworkStat/archive/refs/tags/v1.3.28.tar.gz"
@@ -12,13 +7,11 @@ class Networkstat < Formula
 
   depends_on "python@3.11"
 
-  resource "psutil" do
-    url "https://files.pythonhosted.org/packages/8e/7a/618a0d1f2891d9352891dc0dc291ad05db5e0486d0883ea33c354b3b9de3/psutil-5.9.8.tar.gz"
-    sha256 "081f7c27e478b5cfbe9f9a5ae40d87e30ea914bd16bb7966b648ec371a50188f"
-  end
-
   def install
-    virtualenv_install_with_resources
+    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python3.11/site-packages"
+    system Formula["python@3.11"].opt_bin/"pip3", "install", "psutil", "--target=#{libexec}"
+    bin.install "networkstat.py"
+    (bin/"networkstat").write_env_script libexec/"networkstat.py", PYTHONPATH: ENV["PYTHONPATH"]
     man1.install "networkstat.1"
   end
 
@@ -26,3 +19,4 @@ class Networkstat < Formula
     system "#{bin}/networkstat", "--version"
   end
 end
+
