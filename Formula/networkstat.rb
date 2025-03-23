b0f893d2dd9a79c12830b48caccbc3c38ed0801a
mohamedelleuch777 <usher7med@gmail.com>
2025-03-23 06:05:00 +0100

    Update NetworkStat to v1.3.43

--- Formula/networkstat.rb
+++ Formula/networkstat.rb
@@ -1,8 +1,8 @@
 class Networkstat < Formula
   desc "CLI tool to monitor network interfaces with pretty emoji output"
   homepage "https://github.com/mohamedelleuch777/NetworkStat"
-  url "https://github.com/mohamedelleuch777/NetworkStat/releases/download/v1.3.42/NetworkStat-v1.3.42.tar.gz"
-  sha256 "REPLACE_THIS_WITH_REAL_SHA"
+  url "https://github.com/mohamedelleuch777/NetworkStat/releases/download/v1.3.43/NetworkStat-v1.3.43.tar.gz"
+  sha256 "d9710390cf4d8ad2edee4c653a73e9dd7094ac89735f45b487be4988cee04de4"
   license "Apache-2.0"
 
   depends_on "ldc" # D language compiler
