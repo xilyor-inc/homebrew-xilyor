d8e59758f1c5eb224f813dccae60c4af00002056
Xilyor Owner <135472744+xilyor-owner@users.noreply.github.com>
2025-03-23 05:05:43 +0100

    Update networkstat.rb
    new rb file for D Lang

--- Formula/networkstat.rb
+++ Formula/networkstat.rb
@@ -1,17 +1,12 @@
 class Networkstat < Formula
   desc "CLI tool to monitor network interfaces with pretty emoji output"
   homepage "https://github.com/mohamedelleuch777/NetworkStat"
-  url "https://github.com/mohamedelleuch777/NetworkStat/archive/refs/tags/v1.3.29.tar.gz"
+  url "https://github.com/mohamedelleuch777/NetworkStat/releases/download/v1.3.29/NetworkStat-v1.3.29.tar.gz"
   sha256 "8749b84f512a63f8e5171b52192a51ff082fcd18fbaafe3b315b5549a578baed"
   license "Apache-2.0"
 
-  depends_on "python@3.11"
-
   def install
-    ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python3.11/site-packages"
-    system Formula["python@3.11"].opt_bin/"pip3", "install", "psutil", "--target=#{libexec}"
-    bin.install "dist/networkstat"
-    (bin/"networkstat").write_env_script libexec/"networkstat.py", PYTHONPATH: ENV["PYTHONPATH"]
+    bin.install "networkstat"
     man1.install "networkstat.1"
   end
 
@@ -19,4 +14,3 @@ class Networkstat < Formula
     system "#{bin}/networkstat", "--version"
   end
 end
-
