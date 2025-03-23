32837bae68bacf87e237f1ebc5f7aa54cd91757a
mohamedelleuch777 <usher7med@gmail.com>
2025-03-23 03:50:14 +0100

    Update NetworkStat to v1.3.29

--- Formula/networkstat.rb
+++ Formula/networkstat.rb
@@ -1,8 +1,8 @@
 class Networkstat < Formula
   desc "CLI tool to monitor network interfaces with pretty emoji output"
   homepage "https://github.com/mohamedelleuch777/NetworkStat"
-  url "https://github.com/mohamedelleuch777/NetworkStat/archive/refs/tags/v1.3.28.tar.gz"
-  sha256 "081f7c27e478b5cfbe9f9a5ae40d87e30ea914bd16bb7966b648ec371a50188f"
+  url "https://github.com/mohamedelleuch777/NetworkStat/archive/refs/tags/v1.3.29.tar.gz"
+  sha256 "14906df0c044fdceb27711adb44a736149316a1b04a376c7dfeec97db8d0ad6a"
   license "Apache-2.0"
 
   depends_on "python@3.11"
@@ -10,7 +10,7 @@ class Networkstat < Formula
   def install
     ENV.prepend_create_path "PYTHONPATH", libexec/"lib/python3.11/site-packages"
     system Formula["python@3.11"].opt_bin/"pip3", "install", "psutil", "--target=#{libexec}"
-    bin.install "networkstat.py"
+    bin.install "dist/networkstat"
     (bin/"networkstat").write_env_script libexec/"networkstat.py", PYTHONPATH: ENV["PYTHONPATH"]
     man1.install "networkstat.1"
   end
