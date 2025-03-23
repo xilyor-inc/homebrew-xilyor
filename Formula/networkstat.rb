0c53296d97a34a316833ba5abe6f884a41b27d52
mohamedelleuch777 <usher7med@gmail.com>
2025-03-23 06:02:55 +0100

    Update NetworkStat to v1.3.42

--- Formula/networkstat.rb
+++ Formula/networkstat.rb
@@ -1,16 +1,25 @@
 class Networkstat < Formula
   desc "CLI tool to monitor network interfaces with pretty emoji output"
   homepage "https://github.com/mohamedelleuch777/NetworkStat"
-  url "https://github.com/mohamedelleuch777/NetworkStat/releases/download/v1.3.41/NetworkStat-v1.3.41.tar.gz"
-  sha256 "d9710390cf4d8ad2edee4c653a73e9dd7094ac89735f45b487be4988cee04de4"
+  url "https://github.com/mohamedelleuch777/NetworkStat/releases/download/v1.3.42/NetworkStat-v1.3.42.tar.gz"
+  sha256 "REPLACE_THIS_WITH_REAL_SHA"
   license "Apache-2.0"
 
+  depends_on "ldc" # D language compiler
+
   def install
+    # Compile from source
+    system "ldc2", "-O", "-release", "-boundscheck=off", "-of=networkstat", "main.d"
+
+    # Install compiled binary
     bin.install "networkstat"
-    man1.install "networkstat.1"
+
+    # Optional: install man page
+    man1.install "networkstat.1" if File.exist?("networkstat.1")
   end
 
   test do
     system "#{bin}/networkstat", "--version"
   end
 end
+
